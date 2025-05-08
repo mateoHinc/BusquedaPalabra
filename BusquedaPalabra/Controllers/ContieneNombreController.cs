25473f8500c181da08c7110b080eb92a3f83b63d
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-08 15:17:17 -0500

    Buscar Nombre, Finalizado

---
+++ BusquedaPalabra/Controllers/ContieneNombreController.cs
@@ -0,0 +1,19 @@
+﻿using BusquedaPalabra.Models;
+using BusquedaPalabra.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusquedaPalabra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContieneNombreController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] BusquedaRequest request)
+        {
+            bool resultado = BuscarPalabra.ContieneNombre(request.Info, request.Nombre);
+            return Ok(new { resultado });
+        }
+    }
+}
---
+++ BusquedaPalabra/Models/BusquedaRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BusquedaPalabra.Models
+{
+    public class BusquedaRequest
+    {
+        public string[] Info { get; set; }
+        public string Nombre { get; set; }
+    }
+}
